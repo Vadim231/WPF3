15a7ed2d560f76c51deea48be4980e1ada3ff80e
Vadim231 <Vadim231@github.com>
2019-02-24 09:35:05 +0600

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            textBox1.Cut();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            textBox1.Copy();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            textBox1.Paste();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            grid2.Background = new SolidColorBrush(Colors.Aqua);
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            grid2.Background = new SolidColorBrush(Colors.Red);
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            grid2.Background = new SolidColorBrush(Colors.Yellow);
+        }
+
+        private void Button_MouseMove(object sender, MouseEventArgs e)
+        {
+            Random r = new Random();
+
+            but1.Margin = new Thickness(r.Next(0, 100));
+        }
+    }
+}
